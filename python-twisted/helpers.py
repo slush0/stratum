ad45cc3efa73606a15444642bb066764a655a708
slush <info@bitcoin.cz>
2011-12-19 19:11:48 +0100

    Added call_old_server() for proxying calls to old Electrum servers get_page() now supports HTTP POST

--- python-twisted/helpers.py
+++ python-twisted/helpers.py
@@ -1,6 +1,8 @@
+from zope.interface import implements
 from twisted.internet import defer
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
+from twisted.web.iweb import IBodyProducer
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 
@@ -16,19 +18,50 @@ class ResponseCruncher(Protocol):
         self.response += bytes
 
     def connectionLost(self, reason):
+        print self.response
         self.finished.callback(self.response)
         
+class StringProducer(object):
+    '''Helper for get_page()'''
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return defer.succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
+    
 @defer.inlineCallbacks        
-def get_page(url, method='GET', payload=None):
+def get_page(url, method='GET', payload=None, headers=None):
     '''Downloads the page from given URL, using asynchronous networking'''
     agent = Agent(reactor)
 
+    producer = None
+    if payload:
+        producer = StringProducer(payload)
+
+    _headers = {'User-Agent': [settings.USER_AGENT,]}
+    if headers:
+        for key, value in headers.items():
+            _headers[key] = [value,]
+            
     response = (yield agent.request(
         method,
         str(url),
-        Headers({'User-Agent': [settings.USER_AGENT,]}),
-        payload))
+        Headers(_headers),
+        producer))
 
+    #for h in response.headers.getAllRawHeaders():
+    #    print h
+    
     try:        
         finished = defer.Deferred()
         (yield response).deliverBody(ResponseCruncher(finished))
@@ -36,3 +69,19 @@ def get_page(url, method='GET', payload=None):
         raise Exception("Downloading page '%s' failed" % url)
 
     defer.returnValue((yield finished))
+    
+@defer.inlineCallbacks
+def ask_old_server(method, *args):
+    '''Perform request in old protocol to electrum servers'''
+    import urllib
+    import ast
+    res = (yield get_page('http://ecdsa.org/electrum.php', method='POST',
+                          headers={"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"},
+                          payload=urllib.urlencode({'q': repr([method,] + list(args))})))
+                          
+    try:
+        data = ast.literal_eval(res)
+    except SyntaxError:
+        print "Data received from server:", res
+        raise Exception("Corrupted data from old electrum server")
+    defer.returnValue(data)

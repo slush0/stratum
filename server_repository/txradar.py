12f542ff705b50ce501ce18feea4e4a9e3254f86
slush <info@bitcoin.cz>
2011-12-21 17:04:26 +0100

    Added exception handling to txradar

--- server_repository/txradar.py
+++ server_repository/txradar.py
@@ -11,5 +11,8 @@ class TxradarService(GenericService):
 
     @defer.inlineCallbacks    
     def search(self, tx_hash):
-        result = (yield get_page('http://transactionradar.com/api/v1/tx/%s' % tx_hash))
+        try:
+            result = (yield get_page('http://transactionradar.com/api/v1/tx/%s' % tx_hash))
+        except Exception as exc:
+            raise Exception("Failed to retrieve transaction status: %s" % str(exc))
         defer.returnValue(json.loads(result))

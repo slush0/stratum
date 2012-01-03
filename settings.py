20d572f7d7376e6d24e0e155eb222b43e1e5cc31
slush <info@bitcoin.cz>
2012-01-03 03:46:19 +0100

    Added HTTP_SESSION_TIMEOUT. Added BITCOIN_TRUSTED_HOST

--- settings.py
+++ settings.py
@@ -16,6 +16,9 @@ LISTEN_HTTPSPOLL_TRANSPORT = 8001
 SSL_PRIVKEY = 'privkey.pem'
 SSL_CACERT = 'cacert.pem'
 
+# Keepalive for HTTP transport sessions (at this time for both poll and push)
+HTTP_SESSION_TIMEOUT = 30
+
 # User agent used in HTTP requests (e.g. firstbits lookup)
 USER_AGENT = 'YourServer/0.1'
 
@@ -25,12 +28,17 @@ except:
     print "File 'admin_password' not found, admin functionality in RPC disabled."
     ADMIN_PASSWORD = None # Admin functionality is disabled
 
-'''
-BITCOIN_TRUSTED_HOST = '127.0.0.1'
+try:
+    import signature
+    SIGNING_KEY = signature.load_privkey_pem('signing_key.pem')
+except:
+    print "Loading 'signing_key.pem' failed, protocol messages cannot be signed."
+    SIGNING_KEY = None # Signing is disabled
+
+BITCOIN_TRUSTED_HOST = '109.74.195.194'
 BITCOIN_TRUSTED_PORT = 8332
 BITCOIN_TRUSTED_USER = 'marekp'
 BITCOIN_TRUSTED_PASSWORD = 'brutalniheslo'
-'''
 
 '''
 DATABASE_DRIVER = 'MySQLdb'

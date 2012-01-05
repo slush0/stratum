06f490beb816091284e416f6880d98f549d8d242
slush <info@bitcoin.cz>
2012-01-05 02:20:57 +0100

    Using hashed admin password instead of plaintext

--- server_repository/node.py
+++ server_repository/node.py
@@ -1,5 +1,6 @@
 from twisted.internet import defer
 from twisted.internet import reactor
+import hashlib
 
 import settings 
 from services import GenericService, signature, synchronous
@@ -11,12 +12,12 @@ def admin(func):
         if not len(args):
             raise custom_exceptions.UnauthorizedException("Missing password")
         
-        if not settings.ADMIN_PASSWORD:
+        if not settings.ADMIN_PASSWORD_SHA256:
             raise custom_exceptions.UnauthorizedException("Admin password not set, RPC call disabled")
         
         (password, args) = (args[1], [args[0],] + list(args[2:]))  
 
-        if password != settings.ADMIN_PASSWORD:
+        if hashlib.sha256(password).hexdigest() != settings.ADMIN_PASSWORD_SHA256:
             raise custom_exceptions.UnauthorizedException("Wrong password")
               
         return func(*args, **kwargs)

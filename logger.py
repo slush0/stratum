c03714e7fe0cd003b41928f827d770f0d0da93d1
slush <info@bitcoin.cz>
2012-04-05 16:39:07 +0200

    Remove dependency to settings.py from client-side code.

--- logger.py
+++ logger.py
@@ -3,21 +3,28 @@
 import os
 import logging
 
-import settings
-
+try:
+    import settings
+except ImportError:
+    # This is shared module, but settings are necessary only for server side
+    settings = None
+    
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(getattr(logging, settings.LOGLEVEL))
-    logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
+    if settings:
+        logger.setLevel(getattr(logging, settings.LOGLEVEL))
+        logger.addHandler(file_handler)
+    
     logger.info("Logging initialized")
     return logger
 
 fmt = logging.Formatter("%(asctime)s %(name)s %(module)s.%(funcName)s # %(message)s")
 
-file_handler = logging.FileHandler(os.path.join(settings.LOGDIR, settings.LOGFILE))
-file_handler.setFormatter(fmt)
+if settings:
+    file_handler = logging.FileHandler(os.path.join(settings.LOGDIR, settings.LOGFILE))
+    file_handler.setFormatter(fmt)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(fmt)

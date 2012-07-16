1354d16abe9fba292335867e64ca6b167424a801
slush <info@bitcoin.cz>
2012-07-16 11:40:57 +0200

    Added helper texts and parameters to the methods.

--- example_service.py
+++ example_service.py
@@ -17,33 +17,35 @@ class ExampleService(GenericService):
 
     def hello_world(self):
         return "Hello world!"
+    hello_world.help_text = "Returns string 'Hello world!'"
+    hello_world.params = []
 
     @signature
     def ping(self, payload):
         return payload
+    ping.help_text = "Returns signed message with the payload given by the client."
+    ping.params = [('payload', 'mixed', 'This payload will be sent back to the client.'),]
     
     @synchronous
     def synchronous(self, how_long):
         '''This can use blocking calls, because it runs in separate thread'''
-        for x in range(int(how_long)):
+        for _ in range(int(how_long)):
             time.sleep(1)
         return 'Request finished in %d seconds' % how_long
+    synchronous.help_text = "Run time consuming algorithm in server's threadpool and return the result when it finish."
+    synchronous.params = [('how_long', 'int', 'For how many seconds the algorithm should run.'),]
             
     def throw_exception(self):
         raise Exception("Some error")
+    throw_exception.help_text = "Throw an exception and send error result to the client."
+    throw_exception.params = []
    
     @signature
     def throw_signed_exception(self):
         raise Exception("Some error")
+    throw_signed_exception.help_text = "Throw an exception and send signed error result to the client."
+    throw_signed_exception.params = []
     
-'''
-class TimeSubscription(pubsub.Subscription):
-    def filter(self, t):
-        if t % self.params['period'] == 0:
-            return True
-        return False
-'''
-     
 class TimeSubscription(pubsub.Subscription):
     event = 'example.pubsub.time_event'
     def filter(self, t):
@@ -60,10 +62,14 @@ class PubsubExampleService(GenericService):
     @pubsub.subscribe
     def subscribe(self, period):
         return TimeSubscription(period=period)
+    subscribe.help_text = "Subscribe client for receiving current server's unix timestamp."
+    subscribe.params = [('period', 'int', 'Broadcast to the client only if timestamp%period==0. Use 1 for receiving an event in every second.'),]
     
     @pubsub.unsubscribe
     def unsubscribe(self, period):
         return TimeSubscription(period=period)
+    unsubscribe.help_text = "Stop broadcasting unix timestampt to the client."
+    unsubscribe.params = [('period', 'int', 'Must be the same number as in subscription.'),]
     
     def _emit_time_event(self):
         # This will emit a publish event,

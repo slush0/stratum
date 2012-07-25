a3e9a428e4774ac2485d5c0dc48aff518acfb215
slush <info@bitcoin.cz>
2012-07-25 02:19:10 +0200

    Setup script for 'stratum' package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Stratum',
+      version='0.1',
+      description='Stratum server implementation based on Twisted',
+      author='slush',
+      author_email='info@bitcion.cz',
+      url='http://stratum.bitcoin.cz',
+      packages=['stratum',],
+     )

3dd75217cf8972a5c2ded706935097a1ba67e919
slush <info@bitcoin.cz>
2012-07-25 02:10:09 +0200

    Some testing trash

--- client.py
+++ client.py
@@ -1,18 +1,32 @@
 from twisted.internet import reactor
 from twisted.internet import defer
 
-import custom_exceptions
-from socket_transport import SocketTransportClientFactory
-    
+#import custom_exceptions
+from stratum.socket_transport import SocketTransportClientFactory
+from stratum.services import GenericService
+
 import time
 
+class PubsubReceivingService(GenericService):
+    service_type = 'example.pubsub'
+    service_vendor = 'custom'
+    is_default = True
+    
+    def time_event(self, t):
+        print "Received notification for the time_event !!!", t
+        
+@defer.inlineCallbacks
+def unsubscribe(f, event, key):
+    print "Unsubscribed:", (yield f.rpc('example.pubsub.unsubscribe', [key,]))
+    reactor.stop()
+    
 @defer.inlineCallbacks
 def main():
     debug = False
 
     # Try to connect to remote server
     d = defer.Deferred()
-    hostname = 'london.stratum.bitcoin.cz'
+    hostname = 'california.stratum.bitcoin.cz'
     #hostname = 'localhost'
     f = SocketTransportClientFactory(hostname, 3333,
                 allow_trusted=True,
@@ -23,7 +37,15 @@ def main():
                 on_connect=d)
     yield d # Wait to on_connect event
 
+    (event, subscription_key) = (yield f.rpc('example.pubsub.subscribe', [1,]))
+    print "Subscribed:", event, subscription_key
+    #reactor.callLater(3, unsubscribe, f, event, subscription_key)
+    
+    #print (yield f.rpc('discovery.list_services', []))
+    #print (yield f.rpc('discovery.list_methods', ['example']))
+    #print (yield f.rpc('discovery.list_params', ['example.ping']))
 
+    print (yield f.rpc('example.ping', ['testing payload']))
     '''
     s = time.time()
     x = 10
@@ -34,6 +56,7 @@ def main():
     '''
     #print (yield f.rpc('blockchain.transaction.broadcast', ['01000000016e71be76a49eac1d1f55113e4a581ea21a33e94a17453f6a0f3624db0b43b101000000008b48304502207e1fd0a8a8051fb21561df5d9d0e3ad0f3ced2b51e90ff1cebc85406a654cfaf022100bc7d3cf9a93959ff80953272d45fbd5cbdb8800874db0858783547ecef19c2ce014104e6a069738d8e8491a8abd3bed7d303c9b2dc3792173a18483653036fd74a5100fc6ee327b6a82b3df79005f101b88496988fa414af32df11fff3e96d53d26d03ffffffff0180969800000000001976a914e1c9b052561cf0a1da9ee3175df7d5a2d7ff7dd488ac00000000']))
 
+    '''
     print (yield f.rpc_multi([
                  ['blockchain.block[Electrum].get_blocknum', [], False],
                  ['node.get_peers', [], False],
@@ -41,7 +64,9 @@ def main():
                  ['firstbits.create', ['1MarekMKDKRb6PEeHeVuiCGayk9avyBGBB',], False],
                  ['node.ping', ['test'], False],
                 ]))
+    '''
 
+    '''
     print (yield f.rpc('blockchain.block.get_blocknum', []))
     #print (yield f.rpc('node.get_peers', []))
 
@@ -60,7 +85,8 @@ def main():
     # Example call of firstbits service
     print (yield f.rpc('firstbits.resolve', ['1MarekM',]))
     print (yield f.rpc('firstbits.create', ['1MarekMKDKRb6PEeHeVuiCGayk9avyBGBB',]))
-    reactor.stop()
+    '''
+    #reactor.stop()
     
 main()
 reactor.run()
--- client2.py
+++ client2.py
@@ -1,10 +1,20 @@
 '''The simplest non-twisted client using HTTP polling'''
 import urllib2
+import time
+
+n = 1
+#data = '{"id": 1, "method": "example.pubsub.subscribe", "params": [1]}'+"\n"
+data = '{"id": 2, "method": "example.ping", "params": ["cus"]}'+"\n"
+#data += '{"id": 3, "method": "example.synchronous", "params": [5]}'+"\n"
+#data += '{"id": 4, "method": "example.synchronous", "params": [5]}'+"\n"
+#data += '{"id": 5, "method": "example.synchronous", "params": [5]}'+"\n"
  
+''' 
 data = '{"id": 1, "method": "blockchain.block.ping", "params": ["test"]}'+"\n"
 data += '{"id": 2, "method": "firstbits.resolve", "params": ["1marek"]}'+"\n"
 data += '{"id": 3, "method": "blockchain.block.ping", "params": ["test2"]}'+"\n"
 data += '{"id": 4, "method": "txradar.lookup", "params": ["202e0d0ef7b1299a2193a7aa7bc58161789d72b62948b6005f2a4f190302740c"]}'+"\n"
+'''
 
 try:
     headers = {'cookie': open('cookie.txt', 'r').read().strip(),}
@@ -12,13 +22,19 @@ except:
     headers = {}
     
 headers['content-type'] = 'application/stratum'
+#headers['x-callback-url'] = 'http://localhost:20000'
 
-r = urllib2.Request('http://localhost:8000', data, headers)
-resp = urllib2.urlopen(r)
+s = time.time()
 
-for h in resp.headers:
-    print h, resp.headers[h]
-    if h == 'set-cookie':
-        open('cookie.txt', 'w').write(resp.headers[h])
+for x in range(n):
+    r = urllib2.Request('http://california.stratum.bitcoin.cz:8000', data, headers)
+#    r = urllib2.Request('http://localhost:8000', data, headers)
+    resp = urllib2.urlopen(r)
+
+    for h in resp.headers:
+        print h, resp.headers[h]
+        if h == 'set-cookie':
+            open('cookie.txt', 'w').write(resp.headers[h])
         
-print resp.read()
+    print resp.read()
+print float(n) / (time.time() - s)

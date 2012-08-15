223149f3366bdcdad72b9709a6379d36e56444bc
slush <info@bitcoin.cz>
2012-08-15 23:19:19 +0200

    GenericEventHandler throws standardized exception instead of NotImplemented

--- stratum/event_handler.py
+++ stratum/event_handler.py
@@ -1,4 +1,6 @@
+import custom_exceptions
+
 class GenericEventHandler(object):
     def handle_event(self, msg_method, msg_params, connection_ref):
         print "Other side called method", msg_method, "with params", msg_params
-        raise NotImplemented
+        raise custom_exceptions.MethodNotFoundException("Method '%s' not implemented" % msg_method)

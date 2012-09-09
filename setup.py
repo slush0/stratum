11805b4ca49998e05a05fe06e03e03602ee2c959
slush <info@bitcoin.cz>
2012-09-09 03:33:53 +0200

    version 0.2.2, Windows specific bugfixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ use_setuptools()
 from setuptools import setup
 
 setup(name='stratum',
-      version='0.2.1',
+      version='0.2.2',
       description='Stratum server implementation based on Twisted',
       author='slush',
       author_email='info@bitcion.cz',

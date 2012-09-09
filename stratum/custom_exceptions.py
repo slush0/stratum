a2fe6bd5e04bf1552403edfcbe9edcb0b16b7083
slush <info@bitcoin.cz>
2012-09-09 03:01:27 +0200

    Added codes to custom exceptions

--- stratum/custom_exceptions.py
+++ stratum/custom_exceptions.py
@@ -1,50 +1,62 @@
 class ProtocolException(Exception):
     pass
 
+class TransportException(Exception):
+    pass
+
 class ServiceException(Exception):
+    code = -2
+
+class UnauthorizedException(ServiceException):
     pass
 
-class UnauthorizedException(Exception):
-    pass
-
-class SignatureException(Exception):
-    pass
+class SignatureException(ServiceException):
+    code = -21
 
 class PubsubException(ServiceException):
+    pass
+
+class AlreadySubscribedException(PubsubException):
     pass
 
 class IrcClientException(Exception):
     pass
 
 class SigningNotAvailableException(SignatureException):
-    pass
+    code = -21
 
 class UnknownSignatureIdException(SignatureException):
-    pass
+    code = -22
 
 class UnknownSignatureAlgorithmException(SignatureException):
-    pass
+    code = -22
 
 class SignatureVerificationFailedException(SignatureException):
+    code = -23
+    
+class MissingServiceTypeException(ServiceException):
+    code = -2
+
+class MissingServiceVendorException(ServiceException):
+    code = -2
+
+class MissingServiceIsDefaultException(ServiceException):
+    code = -2
+
+class DefaultServiceAlreadyExistException(ServiceException):
+    code = -2
+
+class ServiceNotFoundException(ServiceException):
+    code = -2
+
+class MethodNotFoundException(ServiceException):
+    code = -3
+    
+class FeeRequiredException(ServiceException):
+    code = -10
+
+class TimeoutServiceException(ServiceException):
     pass
 
 class RemoteServiceException(Exception):
-    pass
-
-class MissingServiceTypeException(ServiceException):
-    pass
-
-class MissingServiceVendorException(ServiceException):
-    pass
-
-class MissingServiceIsDefaultException(ServiceException):
-    pass
-
-class DefaultServiceAlreadyExistException(ServiceException):
-    pass
-
-class ServiceNotFoundException(ServiceException):
-    pass
-
-class MethodNotFoundException(ServiceException):
     pass

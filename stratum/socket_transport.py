21eca6cd729a1905e4e7033570fdfecd1d11bebc
slush <info@bitcoin.cz>
2012-09-09 03:08:40 +0200

    Fixed on_* events, changed constructor API

--- stratum/socket_transport.py
+++ stratum/socket_transport.py
@@ -1,7 +1,8 @@
 from twisted.internet.protocol import ServerFactory
 from twisted.internet.protocol import ReconnectingClientFactory
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 
+import custom_exceptions
 from protocol import Protocol, ClientProtocol
 from event_handler import GenericEventHandler
 
@@ -19,15 +20,15 @@ class SocketTransportFactory(ServerFactory):
 class SocketTransportClientFactory(ReconnectingClientFactory):
     def __init__(self, host, port, allow_trusted=True, allow_untrusted=False,
                  debug=False, signing_key=None, signing_id=None,
-                 on_connect=None, on_disconnect=None, is_reconnecting=True,
+                 is_reconnecting=True,
                  event_handler=GenericEventHandler):
         self.debug = debug
         self.is_reconnecting = is_reconnecting
         self.signing_key = signing_key
         self.signing_id = signing_id
         self.client = None # Reference to open connection
-        self.on_connect = on_connect
-        self.on_disconnect = on_disconnect
+        self.on_disconnect = defer.Deferred()
+        self.on_connect = defer.Deferred()
         self.peers_trusted = {}
         self.peers_untrusted = {}
         self.main_host = (host, port)
@@ -35,8 +36,8 @@ class SocketTransportClientFactory(ReconnectingClientFactory):
         self.event_handler = event_handler
         self.protocol = ClientProtocol
         self.after_connect = []
-        
-        self.timeout_handler = reactor.callLater(10, self.connection_timeout)
+                    
+        self.timeout_handler = reactor.callLater(30, self.connection_timeout)
         reactor.connectTCP(host, port, self)
 
     '''
@@ -59,15 +60,18 @@ class SocketTransportClientFactory(ReconnectingClientFactory):
         if self.client:
             return
         
-        e = Exception("SocketTransportClientFactory connection timed out")
-        if self.on_connect:
-            self.on_connect.errback(e)
+        e = custom_exceptions.TransportException("SocketTransportClientFactory connection timed out")
+        if not self.on_connect.called:
+            d = self.on_connect
+            self.on_connect = defer.Deferred()
+            d.errback(e)
+            
         else:
             raise e
         
     def rpc(self, method, params, *args, **kwargs):
         if not self.client:
-            raise Exception("Not connected")
+            raise custom_exceptions.TransportException("Not connected")
         
         return self.client.rpc(method, params, *args, **kwargs)
     
@@ -78,7 +82,7 @@ class SocketTransportClientFactory(ReconnectingClientFactory):
         on restored connection.
         '''
         if not self.client:
-            raise Exception("Not connected")
+            raise custom_exceptions.TransportException("Not connected")
         
         self.after_connect.append((method, params))
         return self.client.rpc(method, params, *args, **kwargs)
@@ -86,14 +90,14 @@ class SocketTransportClientFactory(ReconnectingClientFactory):
     def buildProtocol(self, addr):
         self.resetDelay()
         return ReconnectingClientFactory.buildProtocol(self, addr)
-        
+                
     def clientConnectionLost(self, connector, reason):
-        log.debug(reason)
         if self.is_reconnecting:
+            log.debug(reason)
             ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
         
     def clientConnectionFailed(self, connector, reason):
-        log.debug(reason)
         if self.is_reconnecting:
+            log.debug(reason)
             ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)        
         

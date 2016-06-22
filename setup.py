61c261333381307928d103c750c3e2182f5b8ef1
flound1129 <adam@flounder.net>
2016-06-22 00:51:16 -0400

    remove broken distribute

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-from distribute_setup import use_setuptools
-use_setuptools()
-
 #python setup.py sdist upload
 
 from setuptools import setup
@@ -14,7 +11,6 @@ setup(name='stratum',
       author_email='info@bitcion.cz',
       url='http://blog.bitcoin.cz/stratum',
       packages=['stratum',],
-      py_modules=['distribute_setup',],
       zip_safe=False,
       install_requires=['twisted', 'ecdsa', 'autobahn',]
      )
